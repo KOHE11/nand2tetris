71a6a5c7083c5984d89586fab4f34f770a230531
Kohei-Ogino <kokyu9696@gmail.com>
2021-08-06 13:18:57 +0900

    Add setup

---
+++ setup.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "
+alias HardwareSimulator='~/nand2tetris/tools/HardwareSimulator.sh'
+alias CPUEmulator='~/nand2tetris/tools/CPUEmulator.sh'
+alias VMEmulator='~/nand2tetris/tools/VMEmulator.sh'
+alias Assembler='~/nand2tetris/tools/Assembler.sh'
+alias JackCompiler='~/nand2tetris/tools/JackCompiler.sh'
+alias TextComparer='~/nand2tetris/tools/TextComparer.sh'
+" >> ./hoge
