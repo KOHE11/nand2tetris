f86efca84c76d85adeea198769ff5570623312ac
Kohei-Ogino <kokyu9696@gmail.com>
2021-08-11 17:30:45 +0900

    Add alias.bash

--- alias.bash
+++ alias.bash
@@ -7,4 +7,4 @@ alias VMEmulator='~/nand2tetris/tools/VMEmulator.sh'
 alias Assembler='~/nand2tetris/tools/Assembler.sh'
 alias JackCompiler='~/nand2tetris/tools/JackCompiler.sh'
 alias TextComparer='~/nand2tetris/tools/TextComparer.sh'
-"
+" >> ~/.bash_aliases
